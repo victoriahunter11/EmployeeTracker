51e37262fd0effaf2815b4e969780d951ed55000
Victoria Hunter <victoria.jane09@gmail.com>
2020-12-03 17:55:44 -0500

    updating seeds

---
+++ seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (id, name)
+VALUES (1, "Marketing");
+INSERT INTO department (id, name)
+VALUES (2,"Sales");
+INSERT INTO department (id, name)
+VALUES (3, "Accounting");
+INSERT INTO department (id, name)
+VALUES (4, "Engineering");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (11, "Advertising Strategy", 50000, 1);
+INSERT INTO department (id, name)
+VALUES (22,"Acct Manager", 60000, 2);
+INSERT INTO department (id, name)
+VALUES (33, "Accountant", 70000, 3);
+INSERT INTO department (id, name)
+VALUES (44, "Software Engineer", 80000, 4);
+
+
+INSERT INTO role ( first_name, last_name, role_id, manager_id)
+VALUES ( "Janet", "Smith", 10, 1);
+INSERT INTO role ( first_name, last_name, role_id, manager_id)
+VALUES ("Alex","John", 20, 2);
+INSERT INTO role ( first_name, last_name, role_id, manager_id)
+VALUES ("Michael", "Edwin", 30, 3);
+INSERT INTO role ( first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Alice", 40, 4);
+
--- server.js
+++ server.js
@@ -13,5 +13,93 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    runSearch();
-})
+    askQuestions();
+});
+
+const askQuestions = () => {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: 
+            [
+                'View all Employees',
+                'View all Employees by Department',
+                'View all Employees by Manager',
+                'Add Employee',
+                'Remove Employee',
+                'Update Employee Role',
+                'Update Employee Manager',
+                'Exit'
+            ],
+        },
+     
+
+    ])
+
+.then((answer) => {
+    switch (answer.choice) {
+        case "View All Employees":
+            viewAllEmployees();
+            break;
+
+         case "View All Employees By Department":
+                viewAllEmployeesByDepartment();
+                break;
+
+         case "View All Employees By Manager":
+                viewAllEmployeesByManager();
+                break;
+
+         case "Add Employees":
+                 addEmployee();
+                 break;
+
+        case "Remove Employees":
+                removeEmployees();
+                 break;
+
+         case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+                    
+         case "Update Employee Manager":
+                updateEmployeeManager();
+                break;
+        case "exit":
+            connection.end()
+           break;
+                    
+                              
+                
+    }
+
+    }); 
+}
+
+function viewAllEmployees() {
+    connection.query("SELECT * FROM department",
+    function (err,res) {
+        if (err) throw err;
+        
+    askQuestions();
+    });
+};
+
+function viewAllEmployeesByDepartment() {
+    connection.query("SELECT * FROM",
+    function (err, res) {
+        if (err) throw err;
+        askQuestions();
+    })
+}
+
+function viewAllEmployeesByManager() {
+    connection.query("SELECT * FROM",
+    function (err, res) {
+        if (err) throw err;
+        askQuestions();
+    })
+}
