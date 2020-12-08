40bb8039d87d3f23c0df1fa654805f57ffe63f79
Victoria Hunter <victoria.jane09@gmail.com>
2020-12-08 17:43:58 -0500

    Issues with Seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -9,20 +9,20 @@ VALUES (4, "Engineering");
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (11, "Advertising Strategy", 50000, 1);
-INSERT INTO department (id, name)
+INSERT INTO role (id, title, salary, department_id)
 VALUES (22,"Acct Manager", 60000, 2);
-INSERT INTO department (id, name)
+INSERT INTO role (id, title, salary, department_id)
 VALUES (33, "Accountant", 70000, 3);
-INSERT INTO department (id, name)
+INSERT INTO role (id, title, salary, department_id)
 VALUES (44, "Software Engineer", 80000, 4);
 
 
-INSERT INTO role ( first_name, last_name, role_id, manager_id)
-VALUES ( "Janet", "Smith", 10, 1);
-INSERT INTO role ( first_name, last_name, role_id, manager_id)
-VALUES ("Alex","John", 20, 2);
-INSERT INTO role ( first_name, last_name, role_id, manager_id)
-VALUES ("Michael", "Edwin", 30, 3);
-INSERT INTO role ( first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Alice", 40, 4);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (97, "Janet", "Smith", 10, 1);
+INSERT INTO employee (id,  first_name, last_name, role_id, manager_id)
+VALUES (96, "Alex","John", 20, 2);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (98, "Michael", "Edwin", 30, 3);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (99, "Jane", "Alice", 40, 4);
 
